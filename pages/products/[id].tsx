d232e105142d8605bf8d83d21fe83a0661977413
TranYen102 <tranyen18012001@gmail.com>
2022-06-30 16:04:59 +0700

    update

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,14 +1,37 @@
+import { GetServerSidePropsContext, GetStaticPaths, GetStaticProps } from 'next'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, {useEffect} from 'react'
 
-type Props = {}
+type ProductProps = {
+  product:any
+}
 
-const ProductDetail = (props: Props) => {
-    const router = useRouter();
-    console.log('router', router.query);
+const ProductDetail = ({product}: ProductProps) => {
+  if(!product) return null;
+
+    // const router = useRouter();
+    // console.log('router', router.query);
   return (
-    <div>ProductDetail</div>
+    <div>{product.name}</div>
   )
+}
+export const getStaticPaths: GetStaticPaths = async() =>{
+  const data = await (await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products`)).json();
+  const paths =data.map(product => (
+    {params : {id:product.id } }
+  ))
+  return {
+    paths,
+    fallback:false
+  }
+}
+//server
+export const getStaticProps:GetStaticProps<ProductProps> = async (context: GetServerSidePropsContext) =>{
+  const product = await(await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products/${context.params?.id}`)).json();
+  return{
+    props:{product},
+    revalidate:60
+  }
 }
 
 export default ProductDetail
